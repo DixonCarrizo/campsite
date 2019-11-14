8623c20258050ea3a1d7ef514bd1d12b55609d71
Dixon <dixon.carrizo@rappi.com>
2019-11-14 11:01:11 -0500

    Set up

---
+++ src/test/java/com/test/campsite/CampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.campsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
